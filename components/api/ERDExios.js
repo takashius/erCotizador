0f3fb1f7bd10692ec2a20d323bafbbe7f9e84eb1
Erick Hernandez <takashi.onimaru@gmail.com>
2023-11-28 13:17:57 -0400

    Added ERDE to the list of words that cSpell should ignore.

---
+++ components/api/ERDExios.js
@@ -0,0 +1,91 @@
+import axios from "axios";
+import urlJoin from "url-join";
+import getEnvVars from "../../environment";
+import { read } from "../helper/LocalStorage";
+import { locale } from "expo-localization";
+
+const { apiUrl } = getEnvVars();
+
+const DEBUG = true;
+
+const ERDEAxios = axios.create();
+
+// interceptor for outgoing requests
+ERDEAxios.interceptors.request.use(
+  async (config) => {
+    const userToken = await read("userToken");
+    if (userToken) {
+      config.headers.common.Authorization = userToken;
+    }
+    config.headers.common["X-Api-Authorization"] =
+      process.env.API_AUTHORIZATION;
+    config.headers.common["Accept-Language"] = locale;
+    console.log(`headers ${JSON.stringify(config)}`);
+    config.headers.common["Content-type"] = "application/json";
+    config.url = urlJoin(apiUrl, config.url);
+    if (DEBUG) {
+      console.log(config.url);
+      console.log(config.data);
+    }
+    return config;
+  },
+  (error) => {
+    if (DEBUG) {
+      console.log("API CALL UNSUCCESSFUL");
+      console.log(JSON.stringify(error));
+    }
+    return Promise.reject(error);
+  }
+);
+
+// interceptor for incoming responses
+ERDEAxios.interceptors.response.use(
+  (response) => {
+    const responseString = JSON.stringify(response);
+    if (DEBUG) {
+      console.log("API CALL RESPONSE");
+      console.log(responseString);
+    }
+
+    if (response.status === 200 || response.status === 201) {
+      return Promise.resolve(response);
+    } else {
+      return Promise.reject(response);
+    }
+  },
+  (error) => {
+    const errorMsg = JSON.stringify(error.message);
+
+    if (DEBUG) {
+      console.log("API ERROR", errorMsg);
+    }
+
+    if (error.response.status) {
+      switch (error.response.status) {
+        case 400:
+          console.log(
+            "Bad Request - please check the request parameters for correct configuration",
+            errorMsg
+          );
+          break;
+        case 401:
+          console.log("Session Expired", errorMsg);
+          break;
+        case 403:
+          console.log(
+            "Forbidden - the passed auth params do not have permission to view this resource",
+            errorMsg
+          );
+          break;
+        case 404:
+          console.log("Not Found", errorMsg);
+          break;
+        default:
+          console.log("Non 400 response error", errorMsg);
+      }
+      return Promise.reject(error);
+    }
+  }
+);
+
+export default ERDEAxios;
