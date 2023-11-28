2c967920cf9e2468219b90c841ed5fb46f28bb2a
Erick Hernandez <takashi.onimaru@gmail.com>
2023-11-28 13:12:44 -0400

    This commit removes the LocalStorage helper functions.

---
+++ environment.js
@@ -0,0 +1,32 @@
+import Constants from "expo-constants";
+
+const ENV = {
+  dev: {
+    apiUrl: "https://localhost:8080",
+    type: "dev",
+  },
+  staging: {
+    apiUrl: "https://cotizador-dev-tdrr.4.us-1.fl0.io",
+    type: "staging",
+  },
+  prod: {
+    apiUrl: "https://cotizador-dev-tdrr.4.us-1.fl0.io",
+    type: "production",
+  },
+};
+
+const getEnvVars = (env = Constants.expoConfig) => {
+  // if (__DEV__) {
+  //   return ENV.dev;
+  // } else if (env === 'staging') {
+  //   return ENV.staging;
+  // } else if (env === 'production') {
+  //   return ENV.prod;
+  // } else {
+  //   return ENV.staging;
+  // }
+  console.log(env);
+  return ENV.prod;
+};
+
+export default getEnvVars;
