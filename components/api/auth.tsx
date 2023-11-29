da868c4d27cad0feb3e7bc69f07f091e333b31d1
Erick Hernandez <takashi.onimaru@gmail.com>
2023-11-29 16:12:41 -0400

    Added "lastname" to the list of words that cSpell should ignore.

--- components/api/auth.tsx
+++ components/api/auth.tsx
@@ -1,6 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import ERDEAxios from "./ERDEAxios";
 
+export interface UserLogin {
+  name: string;
+  lastname: string;
+  email: string;
+  token: string;
+  _id: string;
+}
+
 export const useLogin = (email: String, password: String) => {
   const query = useQuery({
     queryKey: ["login"],
