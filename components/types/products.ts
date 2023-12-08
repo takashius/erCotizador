e486dddccaaeffd1892903d342e1decbfb924dda
Erick Hernandez <takashi.onimaru@gmail.com>
2023-12-08 08:47:17 -0400

    Update product type to include `created` field

--- components/types/products.ts
+++ components/types/products.ts
@@ -1,17 +1,13 @@
 import { type CreatedShort } from "./general";
 
-export interface Product {
-    name: string;
-    description: string;
-    price: number;
-    iva: boolean;
-    created: CreatedShort;
-    _id: string;
-}
 export interface ProductForm {
     name: string;
     description: string;
     price: number;
     iva: boolean;
     _id: string;
+}
+
+export interface Product extends ProductForm {
+    created: CreatedShort;
 }
