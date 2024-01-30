6da0b1fb76569e44174b485940feb8b51a3534b9
Erick Hernandez <takashi.onimaru@gmail.com>
2024-01-30 11:38:36 -0400

    Add new fields to the quote form.

--- app/(drawer)/home/form.tsx
+++ app/(drawer)/home/form.tsx
@@ -1,7 +1,7 @@
 import { Stack, useLocalSearchParams, useNavigation } from "expo-router";
 import { useState } from "react";
-import { useOptions, Spinner, FormCustomer } from "../../../components";
-import { Box } from "native-base";
+import { useOptions, Spinner, FormCustomer, InputForm } from "../../../components";
+import { Box, Button, VStack } from "native-base";
 import { t } from "i18next";
 
 export default () => {
@@ -9,6 +9,61 @@ export default () => {
   const { post } = params;
   const navigation = useNavigation();
   const [errors, setErrors] = useState({});
+  const defaultData = {
+    title: "",
+    description: "",
+    number: "",
+    id: "",
+  };
+  const [formData, setData] = useState<any>(defaultData);
+
+  const onSubmit = () => {
+  };
+
+  const renderForm = () => (
+    <VStack mx="3">
+      <InputForm
+        data={{
+          name: "title",
+          errors,
+          title: t("title"),
+          placeholder: t("cotiza.placeholder.title"),
+          value: formData.title,
+          formData,
+          setData,
+          require: true,
+          description: t("cotiza.titleDescription"),
+        }}
+      />
+      <InputForm
+        data={{
+          name: "description",
+          errors,
+          title: t("description"),
+          placeholder: t("cotiza.placeholder.description"),
+          value: formData.description,
+          formData,
+          setData,
+        }}
+      />
+      <InputForm
+        data={{
+          name: "number",
+          errors,
+          title: t("number"),
+          placeholder: t("cotiza.placeholder.number"),
+          value: String(formData.number),
+          formData,
+          setData,
+          keyboardType: "number-pad",
+          require: true,
+        }}
+      />
+      <Button bgColor={"blue.500"} rounded={"3xl"} onPress={onSubmit} mt="5">
+        {t("submit")}
+      </Button>
+    </VStack>
+  );
 
   return (
     <Box bg="white" safeArea flex="1">
@@ -21,7 +76,7 @@ export default () => {
           }
         )}
       />
-
+      {renderForm()}
     </Box>
   )
 }
